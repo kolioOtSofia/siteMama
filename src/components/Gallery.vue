c2689ed444e14f0734cceff94c3838d0c5bbf7fb
Nikola Genchev <n.genchev@hotmail.com>
2023-01-04 18:29:59 +0200

    Add stuff

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -2,29 +2,29 @@
   <!-- <script src="../../node_modules/tw-elements/dist/js/index.min.js"></script> -->
 
   <section class="overflow-hidden text-white">
-    <div class="container py-2 mx-auto lg:pt-12 ">
-      <div class="flex flex-wrap -m-1 md:-m-2">
+    <div class="container py-2 mx-auto lg:pt-12">
+      <div class="flex flex-wrap -m-1 md:-m-2" @mouseleave="hideEverything">
         <div
-          v-for="project in refs.projects.value"
+          v-for="(project, index) in refs.projects.value"
           class="flex relative flex-wrap w-1/3"
         >
-          <div class="w-full parent">
+          <div class="w-full parent" @mouseover="show(index)">
             <img
-              @mouseover="show"
-              @mouseleave="isHidden = true"
               alt="gallery"
               class="child object-cover object-center w-full h-full lg relative hover:grayscale"
               :src="project.imagesrc"
             />
           </div>
           <div
-            v-if="!isHidden"
-            class="absolute grid grid-rows-2 grid-flow-col line-up gap-2 flex w-full my-16 justify-center"
+            class="absolute grid grid-rows-2 grid-flow-col line-up gap-2 w-full my-16 justify-center"
+            :class="{ hide: isHiddenArray[index] }"
           >
-            <div class="flex w-full justify-center text-white-700">{{ project.title }}</div>
-            <div class="flex w-full justify-center">
+            <p class="flex w-full justify-center text-white-700">
+              {{ project.title }}
+            </p>
+            <p class="flex w-full justify-center">
               {{ project.subtitleText }}
-            </div>
+            </p>
           </div>
         </div>
       </div>
@@ -41,19 +41,36 @@ const props = defineProps<{
 
 const refs = toRefs(props);
 
+const isHiddenArray = ref(
+  Array.from(Array(refs.projects.value.length), () => true)
+);
+
 type Project = {
   imagesrc: string;
   title: string;
   subtitleText: string;
 };
 
-let isHidden = ref(true);
-function show() {
-  if (isHidden.value === true) isHidden.value = false;
+function show(index: number) {
+  const currentlyVisible = isHiddenArray.value.findIndex(
+    (bool) => bool === false
+  );
+  if (currentlyVisible >= 0) isHiddenArray.value[currentlyVisible] = true;
+  if (isHiddenArray.value[index] === true) isHiddenArray.value[index] = false;
+}
+
+function hideEverything() {
+  for (let i = 0; i < isHiddenArray.value.length; i++) {
+    isHiddenArray.value[i] = true;
+  }
 }
 </script>
 
 <style>
+.hide {
+  display: none;
+}
+
 .line-up {
   animation: 2s anim-line-up ease-out forwards;
 }
@@ -83,43 +100,44 @@ function show() {
   cursor: pointer;
 }
 
-
 .child {
-    height: 100%;
-    width: 100%;
-    background-size: cover;
-    background-repeat: no-repeat;
-    -webkit-transition: all .5s;
-    -moz-transition: all .5s;
-    -o-transition: all .5s;
-    transition: all .5s;
+  height: 100%;
+  width: 100%;
+  background-size: cover;
+  background-repeat: no-repeat;
+  -webkit-transition: all 0.5s;
+  -moz-transition: all 0.5s;
+  -o-transition: all 0.5s;
+  transition: all 0.5s;
 }
 
-.parent:hover .child, .parent:focus .child {
-    -ms-transform: scale(1.2);
-    -moz-transform: scale(1.2);
-    -webkit-transform: scale(1.2);
-    -o-transform: scale(1.2);
-    transform: scale(1.2);
+.parent:hover .child,
+.parent:focus .child {
+  -ms-transform: scale(1.2);
+  -moz-transform: scale(1.2);
+  -webkit-transform: scale(1.2);
+  -o-transform: scale(1.2);
+  transform: scale(1.2);
 }
 
-.parent:hover .child:before, .parent:focus .child:before {
-    display: block;
+.parent:hover .child:before,
+.parent:focus .child:before {
+  display: block;
 }
 
-.parent:hover a, .parent:focus a {
-    display: block;
+.parent:hover a,
+.parent:focus a {
+  display: block;
 }
 
 .child:before {
-    content: "";
-    display: none;
-    height: 100%;
-    width: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-    background-color: rgba(52,73,94,0.75);
+  content: "";
+  display: none;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(52, 73, 94, 0.75);
 }
-
 </style>
